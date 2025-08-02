082b10382870856d3811b57a4a633cfa4e313582
Frederick <zf2056@nyu.edu>
2025-08-02 13:04:41 -0700

    add range into enum

--- src/main/java/Grade.java
+++ src/main/java/Grade.java
@@ -1,3 +1,15 @@
 public enum Grade {
-    A, B, C, D, F
+    A(90, 100),
+    B(80, 89),
+    C(70, 79),
+    D(60, 69),
+    F(0, 59);
+
+    public double minumumPercentage;
+    public double maximumPercentage;
+
+    Grade(double mininumPercentage, double maximumPercentage) {
+        this.maximumPercentage = maximumPercentage;
+        this.minumumPercentage = mininumPercentage;
+    }
 }
